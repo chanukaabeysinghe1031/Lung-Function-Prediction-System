55b6ca2655daa267ac5c5be76e128ee63fd6e115
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2021-02-24 20:50:01 +0530

    App
    app

--- App/myapp.py
+++ App/myapp.py
@@ -5,12 +5,12 @@ Created on Wed Feb 24 17:43:33 2021
 @author: Chanuka Abeysinghe
 """
 
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello World</h1>"
+    return render_template('healthy_lung.html')
 
 if __name__ =="__main__":
     app.run(debug=True)
