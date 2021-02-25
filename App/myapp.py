541072027b5c65b17190cce51588871f1b6a8f44
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2021-02-25 06:59:57 +0530

    Flask backend
    Finalized flask backend

--- App/myapp.py
+++ App/myapp.py
@@ -5,7 +5,10 @@ Created on Wed Feb 24 17:43:33 2021
 @author: Chanuka Abeysinghe
 """
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route('/')
---
+++ App/predict_app.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 22:20:39 2021
+
+@author: Chanuka Abeysinghe
+"""
+
+import base64
+import numpy as np
+import io
+from PIL import Image
+import keras
+from keras import backend as k
+from keras.models import Sequential
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from flask import request 
+from flask import jsonify
+from flask import Flask
+import cv2
+
+app = Flask(__name__)
+
+
+def getModel():
+    global model 
+    model=load_model('my_model.h5')
+    print("*! Model loaded")
+    
+def preprocess(image):
+    IMG_PX_SIZE = 50 
+    NUMBER_OF_SLICES =10
+    resized_images=[]
+    resized_image=cv2.resize(np.array(image.pixel_array),(IMG_PX_SIZE,IMG_PX_SIZE))
+    
+    for singleImage in range(10):
+        resized_images.append(resized_image)
+    
+    resized_images = np.transpose(resized_images, (1,2,0))
+    
+    return resized_images
+
+print(" * Loading custom model")
+getModel()
+
+@app.route("/predict",method=["POST"])
+def predict():
+    message = request.get_json(force=True)
+    encoded= message['image']
+    decoded =  base64.b64decode(encoded)
+    image=Image.open(io.BytesIO(decoded))
+    processedImage=preprocess(image)
+    prediction = model.predict(processedImage).tolist()
+    response = {
+        'Prediction':prediction[0]    
+    }
+    return jsonify(response)
+    
+    
+    
+    
+    
+    
+    
