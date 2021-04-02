e9a1c0eb939aaff6f78ebae2bfb8c91df02a9a9a
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2021-04-02 09:49:40 +0530

    Update

--- App/api.py
+++ App/api.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import os
 
+UPLOAD_FOLDER = "/Users/chanukaabeysinghe/Desktop/FYP/Lung-Function-Prediction-System/App/static"
 app = Flask(__name__)
 @app.route("/", methods=["Get","Post"])
 
 def predict():
-    return render_template("index.html")
+    if request.method == "POST":
+        image= request.files["image"]
+        if image:
+            imageLocation = os.path.join(
+                UPLOAD_FOLDER,
+                image.filename
+            )
+            image.save(imageLocation )
+            return render_template("index.html", prediction=1)
+    return render_template("index.html",prediction=0)
 
 if __name__ == "__main__":
     app.run(port=12000,debug=True )
