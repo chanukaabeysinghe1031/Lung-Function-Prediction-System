621ea60fa528834c1f427b81803fd708ca43e17c
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2021-04-04 17:38:24 +0530

    FINISHED WEB APP VERSION 1

--- App/api.py
+++ App/api.py
@@ -9,38 +9,73 @@ import re
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
+from werkzeug.utils import secure_filename
 #from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 #from keras.models import load_model
 #from keras.preprocessing import image
+import pydicom
+import cv2
 
 from flask import Flask 
 
-UPLOAD_FOLDER = "D:\IIT\4th year\FYP\Lung Fibrosis\Prototype\Lung-Function-Prediction-System\App\static"
+UPLOAD_FOLDER = r"./static"
 
 MODEL_PATH = "D:\IIT\4th year\FYP\Lung Fibrosis\Prototype\Lung-Function-Prediction-System\App\my_model.h5"
 
-model.load_model(MODEL_PATH)
-model._make_predict_function()
+path =  ''
 
 app = Flask(__name__)
-@app.route("/", methods=["Get","Post"])
 
+
+def load_model():
+    global model
+    model = tf.keras.models.load_model('model.h5')
+    print(" * Model loaded!")
+
+def preprocess_data(image_location):
+    print(image_location)
+    ct_dicom = pydicom.read_file(image_location)
+    img = ct_dicom.pixel_array
+    resized_image=cv2.resize(np.array(ct_dicom.pixel_array),(50,50))
+    image=np.array(resized_image)
+    print(image.shape)
+    input=[]
+    for i in  range(10):
+        input.append(image)
+
+    input=np.array(input)
+    resized_images = np.transpose(input, (1, 2, 0))
+
+    print(resized_images.shape)
+    input2=[]
+    input2.append(resized_images)
+    input2=np.array(input2)
+    return input2
+
+
+
+@app.route("/", methods=["Get","Post"])
 def predict():
+    print("LOADING MODEL")
+    load_model()
+    print("MODEL LOADED SUCCESSFULLY")
     if request.method == "POST":
         
         image= request.files["image"]
-        basepath=os.path.dirname(__filr__)
+        basepath=os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath,secure_filename    
+            basepath,'',secure_filename(image.filename)
         )
         if image:
             imageLocation = os.path.join(
                 UPLOAD_FOLDER,
                 image.filename
             )
-            image.save(imageLocation )
-            return render_template("index.html", prediction=1)
+            image.save(imageLocation)
+            model_input=preprocess_data(imageLocation)
+            prediction = model.predict(model_input).tolist()
+            print(prediction)
+            return render_template("index.html", prediction=prediction[0][0])
     return render_template("index.html",prediction=0)
 
 if __name__ == "__main__":
